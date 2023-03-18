3a0fc83b7277628a171473d965b531ed6e379e5e
Insomnium-Eye <insomnium-eye@live.com>
2023-03-18 18:05:24 -0500

    Fixed arrow issues, started implementing Router

--- ieye/src/App.js
+++ ieye/src/App.js
@@ -4,100 +4,50 @@ import logoImage from './images/logo_stroke.png';
 import dgp from './images/DGP_Banner.jpg';
 import Modal from './Modal';
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 
 function App() {
-  const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center center',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%'
-  };
 
-  const headerStyle = {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(0, 0, 0, 1)',
-    zIndex: 1,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '0 20px',
-    height: '60px',
-    ul: {
-      listStyle: 'none',
-      margin: 0,
-      padding: 0,
-      li: {
-        display: 'inline-block',
-        marginRight: '10px',
-        a: {
-          color: 'white',
-        },
-      },
-    },
-  };
+    const [modalOpen, setModalOpen] = useState(false);
 
-  const contentStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    textAlign: 'center',
-    color: 'white',
-    backgroundColor: 'transparent',
-    borderStyle: 'unset',
-    boxShadow: '0px 0px 5px black',
-    background: 'rgba(50, 40, 70, 0.7)',
-    maxWidth: '800px',
-    margin: '0 auto',
-    padding: '50px',
-  };
-  
+    return (
+        <Router>
+        <div className="background">
+            <div className="header">
+                <img src={logoImage} alt="Logo" height="40" />
+                <nav>
+                    <ul className="header-ul">
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/about">About</a>
+                        </li>
+                        <li>
+                            <a href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
 
-  const [modalOpen, setModalOpen] = useState(false);
-
-  return (
-    <div style={backgroundStyle}>
-      <div style={headerStyle}>
-        <img src={logoImage} alt="Logo" height="40" />
-        <nav>
-          <ul style={headerStyle.ul}>
-            <li style={headerStyle.ul.li}>
-              <a href="/">Home</a>
-            </li>
-            <li style={headerStyle.ul.li}>
-              <a href="/about">About</a>
-            </li>
-            <li style={headerStyle.ul.li}>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
-
-      </div>
-      <div className="App" style={{ paddingTop: '60px' }}>
-        <header className="App-header"/>
-        <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
-        <div style={contentStyle}>
-          <h1>New Beginnings</h1>
-          <img src={dgp} alt="Post Image" width="600" /><br/>
-          <i>March 4th, 2023</i>
-          <p>Welcome to our world. After many years of study and hard work, we have decided to rebrand entirely. We are rewriting the entire website using ReactJS to remove clutter and showcase our skills.</p>
-          <p>Our ultimate goal is to showcase our game development and artistic ventures. However, for the time being, we will use this space as a blog to attract clients and investors by showcasing our coding skills. In this post, we're showcasing MODALS and Image Galleries.</p>
-          <p>To do: Fix arrow issue, add captions</p>
-          <button onClick={() => setModalOpen(true)}>View Gallery</button>
-        </div>
-      </div>
-    </div>
-  );
+            </div>
+            <div className="App" style={{ paddingTop: '60px' }}>
+                <header className="App-header" />
+                <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
+                <div className="content">
+                    <h1>New Beginnings</h1>
+                    <img src={dgp} alt="Post Image" width="600" /><br />
+                    <i>March 18th, 2023</i>
+                    <p>Welcome to our world. After many years of study and hard work, we have decided to rebrand entirely. We are rewriting the entire website using ReactJS to remove clutter and showcase our skills.</p>
+                    <p>Our ultimate goal is to showcase our game development and artistic ventures. However, for the time being, we will use this space as a blog to attract clients and investors by showcasing our coding skills. In this post, we're showcasing MODALS and Image Galleries.</p>
+                    <p>Most recent update: Fixed Previous/Next arrows on gallery. </p>
+                    <button onClick={() => setModalOpen(true)}>View Gallery</button>
+                </div>
+            </div>
+            </div>
+            </Router>
+    );
 }
 
 export default App;
--- ieye/src/Gallery.js
+++ ieye/src/Gallery.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+
 import img1 from './images/CK_Headshot.png';
 import img2 from './images/CK_SwordSlash.png';
 import img3 from './images/CK_WhipSlash.png';
@@ -48,132 +49,140 @@ const images = [
 ];
 
 const modalStyle = {
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  zIndex: 2,
-  maxWidth: '90%',
-  maxHeight: '90%',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 2,
+    maxWidth: '90%',
+    maxHeight: '90%',
 };
 
 const imageStyle = {
-  maxWidth: '100%',
-  maxHeight: '100%',
-  objectFit: 'contain',
+    maxWidth: '100%',
+    maxHeight: '100%',
+    objectFit: 'contain',
 };
 
 const arrowStyle = {
-  position: 'absolute',
-  top: '50%',
-  transform: 'translateY(-50%)',
-  cursor: 'pointer',
-  fontSize: '3rem',
-  color: '#fff',
-  zIndex: 3,
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    cursor: 'pointer',
+    fontSize: '3rem',
+    color: '#fff',
+    zIndex: 3,
+    border: '2px solid black',
 };
 
+
 function Gallery() {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const totalImages = images.length;
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const totalImages = images.length;
 
-  const thumbnailStyle = {
-    width: '100%',
-    height: 'auto',
-    objectFit: 'contain',
-    margin: '5px',
-    cursor: 'pointer',
-  };
+    const thumbnailStyle = {
+        width: '100%',
+        height: 'auto',
+        objectFit: 'contain',
+        margin: '5px',
+        cursor: 'pointer',
+    };
 
-  const galleryStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, minmax(100px, 1fr))',
-    gridAutoRows: '100px',
-    gap: '5px',
-    padding: '10px',
-  };
+    const galleryStyle = {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(100px, 1fr))',
+        gridAutoRows: '100px',
+        gap: '5px',
+        padding: '10px',
+    };
 
-  function handleClick(image, index) {
-    setSelectedImage(image);
-    setCurrentIndex(index);
-  }
-  
-  function handleClose() {
-    setSelectedImage(null);
-  }
-  
-  function handleNext() {
-    setCurrentIndex((currentIndex + 1) % totalImages);
-  }
-  
-  function handlePrev() {
-    setCurrentIndex((currentIndex + totalImages - 1) % totalImages);
-  }
-  
-  
-  useEffect(() => {
-  function handleKeyPress(e) {
-    if (e.key === 'ArrowRight') {
-      handleNext();
-    } else if (e.key === 'ArrowLeft') {
-      handlePrev();
-    } else if (e.key === 'Escape') {
-      handleClose();
+    function handleClick(image, index) {
+        setSelectedImage(image);
+        setCurrentIndex(index);
     }
-  }
-  if (selectedImage) {
-    document.addEventListener('keydown', handleKeyPress);
-  }
-  return () => {
-    document.removeEventListener('keydown', handleKeyPress);
-  };
-}, [selectedImage, handleNext, handlePrev, handleClose]);
+
+    function handleClose() {
+        setSelectedImage(null);
+    }
+
+    function handleNext(e) {
+        e.stopPropagation();
+        const nextIndex = (currentIndex + 1) % totalImages;
+        setSelectedImage(images[nextIndex]);
+        setCurrentIndex(nextIndex);
+    }
+
+    function handlePrev(e) {
+        e.stopPropagation();
+        const prevIndex = (currentIndex + totalImages - 1) % totalImages;
+        setSelectedImage(images[prevIndex]);
+        setCurrentIndex(prevIndex);
+    }
 
 
-  return (
-    <>
-      <div style={galleryStyle}>
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image.src}
-            alt={image.alt}
-            style={thumbnailStyle}
-            onClick={() => handleClick(image, index)}
-          />
-        ))}
-      </div>
-      {selectedImage && (
-        <div style={modalStyle} onClick={handleClose}>
-          <div style={{ position: 'relative' }}>
-            <div
-              style={{ ...arrowStyle, left: 0 }}
-              onClick={handlePrev}
-            >
-              &#10094;
+    useEffect(() => {
+        function handleKeyPress(e) {
+            if (e.key === 'ArrowRight') {
+                handleNext();
+            } else if (e.key === 'ArrowLeft') {
+                handlePrev();
+            } else if (e.key === 'Escape') {
+                handleClose();
+            }
+        }
+        if (selectedImage) {
+            document.addEventListener('keydown', handleKeyPress);
+        }
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [selectedImage, handleNext, handlePrev, handleClose]);
+
+
+    return (
+        <>
+            <div style={galleryStyle}>
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image.src}
+                        alt={image.alt}
+                        style={thumbnailStyle}
+                        onClick={() => handleClick(image, index)}
+                    />
+                ))}
             </div>
-            <div
-              style={{ ...arrowStyle, right: 0 }}
-              onClick={handleNext}
-            >
-              &#10095;
-            </div>
-            <img
-              src={selectedImage.src}
-              alt={selectedImage.alt}
-              style={imageStyle}
-            />
-          </div>
-        </div>
-      )}
-    </>
-  );
+            {selectedImage && (
+                <div style={modalStyle} onClick={handleClose}>
+                    <div style={{ position: 'relative' }}>
+                        <div
+                            style={{ ...arrowStyle, left: 0 }}
+                            onClick={handlePrev}
+                        >
+                            &#10094;
+                        </div>
+                        <div
+                            style={{ ...arrowStyle, right: 0 }}
+                            onClick={handleNext}
+                        >
+                            &#10095;
+                        </div>
+                        <img
+                            src={selectedImage.src}
+                            alt={selectedImage.alt}
+                            style={imageStyle}
+                        />
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default Gallery;

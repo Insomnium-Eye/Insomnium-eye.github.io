79f4689b70c4695b49cd688275729d758ae5b6a9
Insomnium-Eye <insomnium-eye@live.com>
2023-03-21 19:19:42 -0500

    Update date to test gh-pages

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
                 <div className="content">
                     <h1>New Beginnings</h1>
                     <img src={dgp} alt="Post Image" width="600" /><br />
-                    <i>March 18th, 2023</i>
+                    <i>March 22nd, 2023</i>
                     <p>Welcome to our world. After many years of study and hard work, we have decided to rebrand entirely. We are rewriting the entire website using ReactJS to remove clutter and showcase our skills.</p>
                     <p>Our ultimate goal is to showcase our game development and artistic ventures. However, for the time being, we will use this space as a blog to attract clients and investors by showcasing our coding skills. In this post, we're showcasing MODALS and Image Galleries.</p>
                     <p>Most recent update: Fixed Previous/Next arrows on gallery. </p>

ecb6bca385bbdd20dda9e953528e35f28dd52e7f
Insomnium-Eye <insomnium-eye@live.com>
2023-04-04 16:33:28 -0500

    Added media queries, changed icon and title.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import logoImage from './images/logo_stroke.png';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 
 import Home from './Home.js';
@@ -8,33 +8,66 @@ import About from './About.js';
 import Contact from './Contact.js';
 
 function App() {
-    return (
-        <div className="background">
-            <div className="header">
-                <img src={logoImage} alt="Logo" height="40" />
-                <nav>
-                    <ul className="header-ul">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <div className='content'>
-                    <Routes>
-                        <Route path="/" element={<Home/>} />
-                        <Route path="/about" element={<About/>}/>
-                        <Route path="/contact" element={<Contact/>}/>  
-                    </Routes>
-                </div>
-            </div>          
+  useEffect(() => {
+    // Get the dropdown button and the dropdown content
+    const dropdownButton = document.querySelector('.dropButton');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    // Add a click event listener to the dropdown button
+    dropdownButton.addEventListener('click', () => {
+      // Toggle the visibility of the dropdown content
+      dropdownContent.classList.toggle('show');
+    });
+
+    // Close the dropdown when the user clicks outside of it
+    window.addEventListener('click', (event) => {
+      if (!event.target.matches('.dropButton')) {
+        dropdownContent.classList.remove('show');
+      }
+    });
+
+    // Remove event listeners when the component unmounts
+    return () => {
+      dropdownButton.removeEventListener('click');
+      window.removeEventListener('click');
+    }
+  }, []);
+
+  return (
+    <div className="background">
+      <header className="header">
+        <img src={logoImage} alt="Logo" height="40" />
+        <nav>
+          <ul className="header-ul">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="dropDown">
+          <button className="dropButton">... <i/></button>
+          <div className="dropdown-content">
+            <a><Link to="/">Home</Link></a>
+            <a><Link to="/about">About</Link></a>
+            <a><Link to="/contact">Contact</Link></a>
+          </div>
         </div>
-    );
+      </header>
+      <div className='content'>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>  
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Contact() {
   return (
     <div className="content-contact">
-      <iframe width="640px" height="800px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__r2tgE9URUNSTFFPQzlNWVNHT1FCQjlGT0NTS0NFUy4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+      <iframe title="contact-form" width="100%" height="800px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__r2tgE9URUNSTFFPQzlNWVNHT1FCQjlGT0NTS0NFUy4u&embed=true" frameBorder="0" marginWidth="0" marginHeight="0" allowFullScreen></iframe>
     </div>
   );
 }
